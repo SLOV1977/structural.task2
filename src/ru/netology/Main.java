c3bcd4ac37cfe5f4ec14dbea321ab2303d6ac784
SLOV1977 <79651481509@yandex.ru>
2022-05-04 00:30:58 +0300

    Добавлены файлы проекта

---
+++ src/ru/netology/Main.java
@@ -0,0 +1,9 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("101", "111"));
+        System.out.println(bins.mult("101", "111"));
+    }
+}
